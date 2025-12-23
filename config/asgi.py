c41bd0924bc04d3068cf1ca366dc2ad260c3ed19
mohamedN2018 <moh24832@gmail.com>
2025-12-23 09:37:00 +0200

    end test not work

--- config/asgi.py
+++ config/asgi.py
@@ -3,16 +3,21 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.auth import AuthMiddlewareStack
 from channels.security.websocket import AllowedHostsOriginValidator
-import chat.routing
+import chat.routing  # استدعاء ملف routing الخاص بالتطبيق
 
+# تحديد إعدادات Django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
+# تكوين تطبيق ASGI
 application = ProtocolTypeRouter({
+    # إعداد الاتصال عبر HTTP
     'http': get_asgi_application(),
+
+    # إعداد الاتصال عبر WebSocket
     'websocket': AllowedHostsOriginValidator(
         AuthMiddlewareStack(
             URLRouter(
-                chat.routing.websocket_urlpatterns
+                chat.routing.websocket_urlpatterns  # جميع روابط WebSocket للتطبيق
             )
         )
     ),
